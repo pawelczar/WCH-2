da8feeb93b1a38a63d2d361940d94e6fb01055a2
Pawel <pawel-czarnecki.o2.pl>
2020-12-29 22:38:17 +0000

    testing mobile menu navigation

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const home = document.querySelector('.header');
+const about = document.querySelector('.about');
+const services = document.querySelector('.services');
+const gallery = document.querySelector('.gallery');
+const subscribe = document.querySelector('.subscribe');
+const blog = document.querySelector('.blog');
+const contact = document.querySelector('.contact');
+
 const hamburger = document.querySelector('.nav__hamburger');
 const navBar = document.querySelector('.nav');
 const menu = document.querySelector('.nav__menu');
 const subscribeForm = document.querySelector('.subscribe__form');
 const header = document.querySelector('.header');
 const menuLinks = document.querySelectorAll('.nav__item > a');
+
+// console.log(menuLinks);
+// for(atr of menuLinks) {
+//     atr.href = "";
+//     console.log(atr);
+// }
+
 
 const prevSubmit = function (e){
     e.preventDefault();
@@ -15,8 +30,24 @@ const activeToggle = function () {
     if(!navBar.classList.contains('active')) {
         navBar.classList.add('active');
     }
-    console.log('click');
-    window.scrollBy(0,-60);
+}
+
+const menuSelection = function (e) {
+    e.preventDefault();
+    let link = e.target.innerText.toLowerCase();
+    console.log(link);
+    switch(link){
+        case 'contact': 
+            console.log(contact);
+            const scroll = contact.offsetTop;
+            console.log(scroll);
+            window.scrollTo(0, scroll);
+            window.scrollBy(0,-60);
+            break;
+        default:
+            console.log("default case from switch");
+    }
+    activeToggle();
 }
 
 document.addEventListener('scroll', function(){
@@ -29,11 +60,14 @@ document.addEventListener('scroll', function(){
     }
 });
 
-subscribeForm.addEventListener('submit',prevSubmit);
 hamburger.addEventListener('click', activeToggle);
 
 for( let i = 0; i < menuLinks.length; i++){
-    menuLinks[i].addEventListener('click', activeToggle);
-    menuLinks[i].addEventListener('click', (e)=> e.preventDefault());
+    menuLinks[i].addEventListener('click', menuSelection);
+}
+
+subscribeForm.addEventListener('submit',prevSubmit);
+function newFunction() {
+    contact.scrollIntoView();
 }
 
