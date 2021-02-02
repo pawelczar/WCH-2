fb78542086ae05d0eaf9c20366870be089f2e09f
Pawel <pawel-czarnecki.o2.pl>
2021-02-02 22:50:22 +0000

    simplified 'menuSelection' function and renamed to 'handleMenuSelectio'

--- script.js
+++ script.js
@@ -20,133 +20,101 @@ let previousScroll = 0;
 const SCROLL_OFFSET = 60;
 const MENU_BAR_OFFSET = 71;
 
-const handleSubmit = function (e){
-    e.preventDefault();
-}
+const handleSubmit = function (e) {
+  e.preventDefault();
+};
 
 const toggleMenu = function () {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-    if(!navBar.classList.contains('active')) {
-        navBar.classList.add('active');
-    }
-}
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+  if (!navBar.classList.contains('active')) {
+    navBar.classList.add('active');
+  }
+};
 
-const scrollToPos = function (link) {
-    console.log(link)
-    let menuTarget = document.getElementById(`${link}`);
-    console.log(menuTarget);
-    const scrollVal =(menuTarget.offsetTop);
-    if (scrollVal < scrollY){
-            window.scrollTo(0, (scrollVal - SCROLL_OFFSET));
-        }else{
-            window.scrollTo(0, (scrollVal));
-            }
-}
+const scrollToPos = function (target) {
+  const scrollVal = target.offsetTop;
+  if (scrollVal < scrollY) {
+    window.scrollTo(0, scrollVal - SCROLL_OFFSET);
+  } else {
+    window.scrollTo(0, scrollVal);
+  }
+};
 
-const menuSelection = function (e) {
-    e.preventDefault();
-    let link = e.target;
-    let id = link.hash
-    scrollToPos(id);
-    // console.log(hash);
-    // let link = e.target.innerText.toLowerCase();
-    // switch(link){
-    //     case 'home':
-    //         scrollToPos(home);
-    //         break;
-    //     case 'about':
-    //         scrollToPos(about);
-    //         break;
-    //     case 'services':
-    //         scrollToPos(services);
-    //         break;
-    //     case 'gallery':
-    //         scrollToPos(gallery);
-    //         break;
-    //     case 'subscribe':
-    //         scrollToPos(subscribe);
-    //         break;
-    //     case 'blog':
-    //         scrollToPos(blog);
-    //         break;
-    //     case 'contact': 
-    //         scrollToPos(contact);
-    //     default:
-    //         console.log("default case from switch");
-    // }
-    toggleMenu();
-}
+const handleMenuSelection = function (e) {
+  e.preventDefault();
+  let link = e.target;
+  let id = link.hash;
+  let menuTarget = document.querySelector(`${id}`);
+  scrollToPos(menuTarget);
+  toggleMenu();
+};
 
 const removeActiveClass = (current) => {
-    current.classList.add('active');
-    for(item of menuItems){
-        if(item.classList.contains('active') && item !== current){
-            item.classList.remove('active');
-
-        }
+  current.classList.add('active');
+  for (item of menuItems) {
+    if (item.classList.contains('active') && item !== current) {
+      item.classList.remove('active');
     }
-}
+  }
+};
 
-const getScrollPos = () =>{
-    scrollPos = scrollY;
-    let current;
-    const homePos = home.getBoundingClientRect();
-    const aboutPos = about.getBoundingClientRect();
-    const servicesPos = services.getBoundingClientRect();
-    const galleryPos = gallery.getBoundingClientRect();
-    const subscribePos = subscribe.getBoundingClientRect();
-    const blogPos = blog.getBoundingClientRect();
-    const contactPos = contact.getBoundingClientRect();
+const getScrollPos = () => {
+  scrollPos = scrollY;
+  let current;
+  const homePos = home.getBoundingClientRect();
+  const aboutPos = about.getBoundingClientRect();
+  const servicesPos = services.getBoundingClientRect();
+  const galleryPos = gallery.getBoundingClientRect();
+  const subscribePos = subscribe.getBoundingClientRect();
+  const blogPos = blog.getBoundingClientRect();
+  const contactPos = contact.getBoundingClientRect();
 
-    if (homePos.height/2 <= aboutPos.y){
-        current = menuItems[0];
-        removeActiveClass(current);
-    }else if(aboutPos.height/2 <= servicesPos.y){
-        current = menuItems[1];
-        removeActiveClass(current);
-    }else if(servicesPos.height/2 <= galleryPos.y){
-        current = menuItems[2];
-        removeActiveClass(current);
-    }else if(galleryPos.height/2 <= subscribePos.y){
-        current = menuItems[3];
-        removeActiveClass(current);
-    }else if(subscribePos.height/2 <= blogPos.y){
-        current = menuItems[4];
-        removeActiveClass(current);
-    }else if(blogPos.height/2 <= contactPos.y){
-        current = menuItems[5];
-        removeActiveClass(current);
-    }else{
-        current = menuItems[6];
-        removeActiveClass(current);
-    }
-}
+  if (homePos.height / 2 <= aboutPos.y) {
+    current = menuItems[0];
+    removeActiveClass(current);
+  } else if (aboutPos.height / 2 <= servicesPos.y) {
+    current = menuItems[1];
+    removeActiveClass(current);
+  } else if (servicesPos.height / 2 <= galleryPos.y) {
+    current = menuItems[2];
+    removeActiveClass(current);
+  } else if (galleryPos.height / 2 <= subscribePos.y) {
+    current = menuItems[3];
+    removeActiveClass(current);
+  } else if (subscribePos.height / 2 <= blogPos.y) {
+    current = menuItems[4];
+    removeActiveClass(current);
+  } else if (blogPos.height / 2 <= contactPos.y) {
+    current = menuItems[5];
+    removeActiveClass(current);
+  } else {
+    current = menuItems[6];
+    removeActiveClass(current);
+  }
+};
 
-window.addEventListener('scroll', function(){
-    getScrollPos();
-    if (window.scrollY < MENU_BAR_OFFSET) {
-        navBar.classList.remove('active');
-        previousScroll = window.scrollY;
+window.addEventListener('scroll', function () {
+  getScrollPos();
+  if (window.scrollY < MENU_BAR_OFFSET) {
+    navBar.classList.remove('active');
+    previousScroll = window.scrollY;
+  } else {
+    navBar.classList.add('active');
+    if (window.scrollY >= previousScroll) {
+      navBar.classList.add('slideIn');
+      previousScroll = window.scrollY;
+    } else {
+      navBar.classList.remove('slideIn');
+      previousScroll = window.scrollY;
     }
-    else
-    {   
-        navBar.classList.add('active');
-                if(window.scrollY >= previousScroll){
-            navBar.classList.add('slideIn');
-            previousScroll = window.scrollY;
-        }else{
-            navBar.classList.remove('slideIn');
-            previousScroll = window.scrollY;
-        }
-    }
+  }
 });
 
 hamburger.addEventListener('click', toggleMenu);
 
-for( let i = 0; i < menuLinks.length; i++){
-    menuLinks[i].addEventListener('click', menuSelection);
+for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener('click', handleMenuSelection);
 }
 
-subscribeForm.addEventListener('submit',handleSubmit);
-
+subscribeForm.addEventListener('submit', handleSubmit);
